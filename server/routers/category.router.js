9464e01d18e1657e270dc983c1f9158299c08acb
sangrimlee <contact@sangrim.dev>
2022-07-21 11:11:57 +0900

    feat: Category Controller 추가
    - Category Controller 추가
    - 모든 카테고리를 가져오는 API 추가
    
    - Close #14

--- server/routers/category.router.js
+++ server/routers/category.router.js
@@ -1,11 +1,8 @@
 import { Router as router } from 'express';
+import categoryController from '../controllers/category.controller';
 
 const categoryRouter = router();
 
-categoryRouter.get('/', (req, res) => {
-  res.status(200).json({
-    statusCode: 200,
-  });
-});
+categoryRouter.get('/', categoryController.getAllCategories);
 
 export default categoryRouter;
