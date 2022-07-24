88a1b48764f0ef7a63c00e50b8de17b35887d029
happyGyu <happyboyto.dev@gmail.com>
2022-07-24 21:49:24 +0900

    feat: dateUtil 구현
    - date 객체를 쉽게 다루도록 도와주는 함수들 구현
    - 해당 달의 첫날, 끝날 찾는 함수
    - 포매팅하는 함수

--- client/src/utils/date-util.js
+++ client/src/utils/date-util.js
@@ -2,6 +2,23 @@ function getYearAndMonth(dateObj) {
   return { year: dateObj.getFullYear(), month: dateObj.getMonth() + 1 };
 }
 
+function convertDateString(dateObj) {
+  return dateObj.toISOString().split('T')[0];
+}
+
+function getFirstDateOfMonth(dateObj) {
+  const { year, month } = getYearAndMonth(dateObj);
+  return new Date(year, month - 1, 1);
+}
+
+function getLastDateOfMonth(dateObj) {
+  const { year, month } = getYearAndMonth(dateObj);
+  return new Date(year, month, 0);
+}
+
 export default {
   getYearAndMonth,
+  convertDateString,
+  getFirstDateOfMonth,
+  getLastDateOfMonth,
 };
