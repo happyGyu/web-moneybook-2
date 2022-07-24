049a4f850c4b3c4d8d8a5ab23d469172a10a66da
happyGyu <happyboyto.dev@gmail.com>
2022-07-24 22:20:06 +0900

    feat: inputBar 컴포넌트 구현
    - input box들을 모아서 그리는 컴포넌트 구현
    - 기본적인 스타일링 작업
    
    - #27

---
+++ client/src/components/InputBar/index.js
@@ -0,0 +1,33 @@
+import Component from '@/base/component';
+import './index.css';
+import DateInput from './DateInput';
+import TitleInput from './TitleInput';
+import CategoryInput from './CategoryInput';
+import PaymentMethodInput from './PaymentMethodInput';
+import MoneyInput from './MoneyInput';
+import ConfirmButton from './ConfirmButton';
+import { STORE_KEYS } from '@/constants/keys';
+
+export default class InputBar extends Component {
+  constructor(parentNode) {
+    super(parentNode, 'form', { class: 'input-bar shadow' });
+    this.activate();
+  }
+
+  render(inputBarData) {
+    if (!inputBarData) return;
+    const { date, title, category, paymentMethod, isIncome, amount } =
+      inputBarData;
+    this.currentNode.innerHTML = '';
+    new ConfirmButton(this.currentNode, inputBarData);
+    new DateInput(this.currentNode, date);
+    new CategoryInput(this.currentNode, category);
+    new TitleInput(this.currentNode, title);
+    new PaymentMethodInput(this.currentNode, paymentMethod);
+    new MoneyInput(this.currentNode, { isIncome, amount });
+  }
+
+  activate() {
+    this.subscribe(STORE_KEYS.INPUT_BAR_DATA);
+  }
+}
