21d0b09ce2a8d9c78db4bfc32ce90d37fb10b6d4
happyGyu <happyboyto.dev@gmail.com>
2022-07-24 21:58:28 +0900

    feat: 금액 입력 박스 구현
    - 부호를 누르면 전환
    - 금액 입력 부분은 input type number
    
    - #27

---
+++ client/src/components/InputBar/MoneyInput/index.js
@@ -0,0 +1,40 @@
+import Component from '@/base/component';
+import controller from '@/controller';
+import { INPUT_BAR_KEYS } from '@/constants/keys';
+
+export default class MoneyInput extends Component {
+  constructor(parentNode, moneyData) {
+    super(parentNode, 'label', { class: 'input-box' }, moneyData);
+    this.moneyData = moneyData;
+    this.activate();
+  }
+
+  render(moneyData) {
+    this.moneyData = moneyData;
+    const { isIncome, amount } = moneyData;
+    this.currentNode.innerHTML = `
+      <h4 class="input__title">금액</h4>
+      <div class="input__money-container">
+        <span class="input input__money-sign" name="money-sign" type="text" data-isincome=${isIncome}>${
+      isIncome ? '+' : '-'
+    }</span>
+        <input class="input input__money-amount" name="amount" type="number" placeholder='입력하세요' value="${
+          amount || ''
+        }">
+        <span>원</span>
+      </div>
+      `;
+  }
+
+  activate() {
+    this.addEvent('change', '.input__money-amount', (event) =>
+      controller.changeInputData(INPUT_BAR_KEYS.AMOUNT, event.target.value),
+    );
+    this.addEvent('click', '.input__money-sign', (event) =>
+      controller.changeInputData(
+        INPUT_BAR_KEYS.IS_INCOME,
+        !this.moneyData.isIncome,
+      ),
+    );
+  }
+}
