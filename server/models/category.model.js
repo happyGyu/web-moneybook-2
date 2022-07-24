ee1139c80826466fc00f5e5cdad26e57275204dc
sangrimlee <contact@sangrim.dev>
2022-07-24 15:54:41 +0900

    feat: Category Model에 findOneBy 추가
    - Category Model에 하나의 Row를 찾는 findOneBy 추가
    
    - #28

--- server/models/category.model.js
+++ server/models/category.model.js
@@ -1,15 +1,24 @@
-import pool from '../db';
+import query from '../db/query';
 
 const categoryModel = {
+  async findOne({ where, or }) {
+    const whereCondition = Object.entries(where)
+      .map(([col, value]) => `${col} = '${value}'`)
+      .join(or ? 'OR' : 'AND');
+    const [category] = await query(`
+      SELECT id, title, color
+      FROM Category
+      ${whereCondition}
+      LIMIT 1;
+    `);
+    return category[0] ?? null;
+  },
+
   async findAll() {
-    const connection = await pool.getConnection();
-    await connection.beginTransaction();
-    const [categories] = await connection.query(`
-          SELECT C.id, C.title, C.color
-          FROM Category as C
-      `);
-    await connection.commit();
-    connection.release();
+    const [categories] = await query(`
+    SELECT id, title, color
+    FROM Category;
+  `);
     return categories;
   },
 };
