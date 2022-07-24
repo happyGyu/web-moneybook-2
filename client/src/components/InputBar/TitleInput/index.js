41d73c12f1d82f8712922ee1479030c3c24a8613
happyGyu <happyboyto.dev@gmail.com>
2022-07-24 21:55:25 +0900

    feat: 내용 입력 박스 구현
    - #27

---
+++ client/src/components/InputBar/TitleInput/index.js
@@ -0,0 +1,24 @@
+import Component from '@/base/component';
+import controller from '@/controller';
+import { INPUT_BAR_KEYS } from '@/constants/keys';
+export default class TitleInput extends Component {
+  constructor(parentNode, titleData) {
+    super(parentNode, 'label', { class: 'input-box' }, titleData);
+    this.activate();
+  }
+
+  render(titleData) {
+    this.currentNode.innerHTML = `
+      <h4 class="input__title">내용</h4>
+      <input class="input input__content" name="title" type="text" placeholder='입력하세요' value="${
+        titleData || ''
+      }">
+    `;
+  }
+
+  activate() {
+    this.addEvent('change', '.input__content', (event) =>
+      controller.changeInputData(INPUT_BAR_KEYS.TITLE, event.target.value),
+    );
+  }
+}
