4eb9a965d36269861fd85b9e55fe7e751ba50019
sangrimlee <contact@sangrim.dev>
2022-07-27 02:13:11 +0900

    feat: CalendarFooter 총계 추가
    - Calendar 총계 추가
    
    - #40

--- client/src/components/Calendar/CalendarFooter/index.js
+++ client/src/components/Calendar/CalendarFooter/index.js
@@ -1,23 +1,24 @@
 import Component from '@/base/component';
 
 export default class CalendarFooter extends Component {
-  constructor(parentNode) {
-    super(parentNode, 'tfoot', { class: 'calendar-footer' });
+  constructor(parentNode, props) {
+    super(parentNode, 'tfoot', { class: 'calendar-footer' }, null, props);
   }
 
   render() {
+    const { totalIncomeAmount, totalSpentAmount } = this.props;
     this.currentNode.innerHTML = `
       <tr class="calendar-footer__income">
         <th>총 수입</th>
-        <td>1,822,480</td>
+        <td>${totalIncomeAmount.toLocaleString()}원</td>
       </tr>
       <tr class="calendar-footer__spent">
         <th>총 지출</th>
-        <td>1,822,480</td>
+        <td>${totalSpentAmount.toLocaleString()}원</td>
       </tr>
       <tr class="calendar-footer__total">
         <th>총계</th>
-        <td>1,822,480</td>
+        <td>${(totalIncomeAmount - totalSpentAmount).toLocaleString()}원</td>
       </tr>
     `;
   }
--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -3,18 +3,30 @@ import Component from '@/base/component';
 import CalendarHeader from './CalendarHeader';
 import CalendarBody from './CalendarBody';
 import CalendarFooter from './CalendarFooter';
+import { STORE_KEYS } from '@/constants/keys';
+import { calculateTotalAmount } from '@/utils/transaction-history-util';
 
 export default class Calendar extends Component {
   constructor(parentNode) {
     super(parentNode, 'table', {
       class: 'calendar container',
-      cellspacing: 0,
     });
+    this.activate();
   }
 
-  render() {
+  activate() {
+    this.subscribe(STORE_KEYS.TRANSACTION_HISTORIES);
+  }
+
+  render(transactionHistories) {
+    if (!transactionHistories) return;
+    const { totalIncomeAmount, totalSpentAmount } =
+      calculateTotalAmount(transactionHistories);
     new CalendarHeader(this.currentNode);
     new CalendarBody(this.currentNode);
-    new CalendarFooter(this.currentNode);
+    new CalendarFooter(this.currentNode, {
+      totalIncomeAmount,
+      totalSpentAmount,
+    });
   }
 }
