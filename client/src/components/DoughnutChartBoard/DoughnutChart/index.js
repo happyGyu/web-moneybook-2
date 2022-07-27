b42a5b07d99fb2b4e94f8ef1597dd2267c7453e6
sangrimlee <contact@sangrim.dev>
2022-07-27 17:39:25 +0900

    feat: 기본적인 도넛 차트 구현
    - 기본적인 도넛 차트 구현

--- client/src/components/DoughnutChartBoard/DoughnutChart/index.js
+++ client/src/components/DoughnutChartBoard/DoughnutChart/index.js
@@ -1,14 +1,32 @@
-import Component from '@/base/component';
+import Chart from '@/base/chart';
 
-export default class DoughnutChart extends Component {
-  constructor(parentNode) {
-    super(parentNode, 'div', { class: 'doughnut-chart' });
-    this.activate();
+export default class DoughnutChart extends Chart {
+  draw(t) {
+    this.clear();
+    const radius = this.canvas.width / 2;
+    const totalValue = this.getTotalValue();
+
+    let prevAngle = 0;
+    for (const data of this.dataset) {
+      const { value, color } = this.getData(data);
+      const currentAngle = prevAngle + (value / totalValue) * 2 * Math.PI * t;
+      this.context.beginPath();
+      this.context.arc(radius, radius, radius, prevAngle, currentAngle);
+      prevAngle = currentAngle;
+      this.context.lineTo(radius, radius);
+      this.context.fillStyle = color;
+      this.context.fill();
+    }
+
+    this.clearCircle(radius, radius, radius * 0.5);
   }
 
-  render() {
-    this.currentNode.innerHTML = 'DOUGHNUT_CHART';
+  clearCircle(x, y, radius) {
+    this.context.save();
+    this.context.beginPath();
+    this.context.arc(x, y, radius, 0, 2 * Math.PI, true);
+    this.context.clip();
+    this.context.clearRect(x - radius, y - radius, radius * 2, radius * 2);
+    this.context.restore();
   }
-
-  activate() {}
 }
