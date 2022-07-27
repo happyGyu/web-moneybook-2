530bb2626d6380f9c4751330fd8e1b7bad316ad6
sangrimlee <contact@sangrim.dev>
2022-07-27 17:38:46 +0900

    feat: BaseChart 구현
    - 기본적인 Chart 구현

---
+++ client/src/base/chart.js
@@ -0,0 +1,59 @@
+export default class Chart {
+  constructor(canvas, dataset, options) {
+    this.canvas = canvas;
+    this.dataset = dataset;
+    this.options = { aspectRatio: 1, ...options };
+    this.context = this.canvas.getContext('2d');
+    this.canvas.height = this.canvas.width * this.options.aspectRatio;
+    this.setDatabaseKey();
+  }
+
+  setDatabaseKey() {
+    const originKey = {
+      label: 'label',
+      value: 'value',
+      color: 'color',
+    };
+    this.key = {
+      ...originKey,
+      ...this.options.key,
+    };
+  }
+
+  getData(dataset, key) {
+    const data = {
+      label: dataset[this.key.label],
+      value: dataset[this.key.value],
+      color: dataset[this.key.color],
+    };
+    return key ? data[key] : data;
+  }
+
+  getTotalValue() {
+    return this.dataset.reduce(
+      (prev, data) => prev + this.getData(data, 'value'),
+      0,
+    );
+  }
+
+  clear() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  draw() {}
+
+  startDraw(ratio = 1, func = (value) => value) {
+    const startAnimation = () => {
+      let t = 0;
+      const animation = () => {
+        t += ratio;
+        this.draw(Math.min(1, func(t)));
+        if (t < 1) {
+          requestAnimationFrame(animation);
+        }
+      };
+      return animation;
+    };
+    startAnimation()();
+  }
+}
