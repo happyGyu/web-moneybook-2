bc38aaebdcdd82cc013bf256c6c7d7f00bd26626
sangrimlee <contact@sangrim.dev>
2022-07-28 09:18:02 +0900

    feat: CREATE TABLE SQL문 실행 추가
    - 처음 시작때 TABLE이 없다면 생성하도록 수정
    
    - #49

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,6 @@
 import mysql from 'mysql2/promise';
 import config from '../config';
+import init from './init';
 
 const pool = mysql.createPool({
   host: config.DB_HOST,
@@ -7,5 +8,9 @@ const pool = mysql.createPool({
   database: config.DB_NAME,
   password: config.DB_PASSWORD,
 });
+
+(async () => {
+  await init(pool);
+})();
 
 export default pool;
