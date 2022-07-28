3bb2811f7eeac6e0abefcd89a751704b811e50c4
happyGyu <happyboyto.dev@gmail.com>
2022-07-28 09:34:26 +0900

    fix: 지출 금액이 0이면 0%로 표기

--- client/src/components/StatisticsBoard/DoughnutChartBoard/SpentList/index.js
+++ client/src/components/StatisticsBoard/DoughnutChartBoard/SpentList/index.js
@@ -24,13 +24,13 @@ export default class SpentList extends Component {
 
   getListItemTemplate(totalSpentAmountData, monthTotalSpentAmount) {
     const { id, title, color, totalSpentAmount } = totalSpentAmountData;
-
+    const spentPercentage = monthTotalSpentAmount
+      ? Math.round(totalSpentAmount / monthTotalSpentAmount) * 100
+      : 0;
     return `
       <li class="spent-list-item" data-id="${id}" data-title="${title}">
         <div class="spent-list-item__category" style="background: ${color}">${title}</div>
-        <span class="spent-list-item__ratio">${Math.round(
-          (totalSpentAmount / monthTotalSpentAmount) * 100,
-        )}%</span>
+        <span class="spent-list-item__ratio">${spentPercentage}%</span>
         <span class="spent-list-item__spent">${totalSpentAmount.toLocaleString()}</span>
       </li>
     `;
