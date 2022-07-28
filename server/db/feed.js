edeb2fdbc6442d11251a6fc56d5dbbe1245863fd
sangrimlee <contact@sangrim.dev>
2022-07-28 10:18:53 +0900

    feat: 자동으로 결제 내역 추가하는 기능 추가
    - development에서만 자동으로 결제 내역을 추가하는 기능 추가
    
    - #49

--- server/db/feed.js
+++ server/db/feed.js
@@ -1,3 +1,23 @@
+import config from '../config';
+
+function getRandomValue(minValue, maxValue) {
+  return Math.round(Math.random() * (maxValue - minValue) + minValue);
+}
+
+function getRandomValueWithExclude(minValue, maxValue, excludeValue) {
+  let randomValue = getRandomValue(minValue, maxValue);
+  while (randomValue === excludeValue) {
+    randomValue = getRandomValue(minValue, maxValue);
+  }
+  return randomValue;
+}
+
+function getRandomDate(minDate, maxDate) {
+  return new Date(
+    Math.round(Math.random() * (maxDate - minDate)) + minDate.getTime(),
+  );
+}
+
 export default async function feed(pool) {
   const CATEGORIES = [
     { title: '월급', color: '#B9D58C', isIncome: true },
@@ -22,7 +42,7 @@ export default async function feed(pool) {
   const connection = await pool.getConnection();
   await Promise.all(
     CATEGORIES.map(({ title, color, isIncome }) => {
-      connection.query(
+      return connection.query(
         `
         INSERT INTO Category (title, color, isIncome)
         SELECT * FROM (SELECT ?, ?, ?) AS Temp
@@ -33,21 +53,48 @@ export default async function feed(pool) {
       );
     }),
   );
+  console.log('DB: FINISH INSERT CATEGORY');
 
-  await Promise.all(
-    PAYMENT_METHODS.map(({ title }) => {
-      connection.query(
-        `
+  if (!config.IS_PRODUCTION) {
+    await Promise.all(
+      PAYMENT_METHODS.map(({ title }) => {
+        return connection.query(
+          `
         INSERT INTO PaymentMethod ( title )
         SELECT * FROM ( SELECT ? ) AS Temp
         WHERE NOT EXISTS (
             SELECT title FROM PaymentMethod WHERE title = ?
         ) LIMIT 1;`,
-        [title, title],
-      );
-    }),
-  );
+          [title, title],
+        );
+      }),
+    );
 
-  console.log('DB: INSERT DATA');
+    await Promise.all(
+      new Array(100).fill(undefined).map((_, i) => {
+        const title = `History${i}`;
+        const minDate = new Date(2021, 10, 1);
+        const maxDate = new Date(2022, 7, 28);
+
+        const date = getRandomDate(minDate, maxDate);
+        const isIncome = Math.random() < 0.25;
+        const amount = getRandomValue(10000, 1000000);
+        const categoryId = isIncome
+          ? getRandomValue(1, 3)
+          : getRandomValue(4, 10);
+        const paymentMethodId = getRandomValueWithExclude(1, 4, categoryId);
+
+        return connection.query(
+          `
+          INSERT INTO TransactionHistory ( title, date, isIncome, amount, categoryId, paymentMethodId)
+          SELECT * FROM ( SELECT ?, ?, ?, ?, ?, ? ) AS Temp
+          WHERE NOT EXISTS ( SELECT title FROM PaymentMethod WHERE title = ? ) LIMIT 1;`,
+          [title, date, isIncome, amount, categoryId, paymentMethodId, title],
+        );
+      }),
+    );
+    console.log('DB: FINISH INSERT PAYMENT_METHOD AND TRANSACTION_HISTORY');
+  }
+  console.log('DB: FINISH INSERT DATA');
   connection.release();
 }
